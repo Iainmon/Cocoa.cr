57fab3b65edf7056518f40855b3c47d8555b7730
Iain Moncrief <Iainmoncrief@gmail.com>
2019-04-17 10:59:19 -1000

    deleted old files

--- src/app/ui/resources/scripts/wget.cr
+++
@@ -1,5 +0,0 @@
-module Scripts
-    SNATCH_SCRIPT = ->(domain : String) {
-        "ROOTPATH=~/Documents; wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains #{domain} --no-parent #{domain} --directory-prefix=$ROOTPATH"
-    }
-end
